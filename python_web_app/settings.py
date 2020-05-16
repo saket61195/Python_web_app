bdecc950bba2932a3296a4157bcc45c8bc5b8788
saket61195 <saket61195@gmail.com>
2020-05-17 02:59:18 +0530

    configure database postgres

--- python_web_app/settings.py
+++ python_web_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'python_web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "travellodb",
+        'USER': "django_dev",
+        'PASSWORD': "adware@123",
+        'HOST': "localhost",
+        'PORT': 5432
     }
 }
 
